8a58ba2922a298cab1f5330b9f6b2b3a3f501a76
tomjimseng <tomjimseng@gmail.com>
2020-02-03 22:08:33 -0800

    tommy and i did it

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,22 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
+var getElementsByClassName = function(className, node) {
   // your code here
+  var Arr = [];
+
+  node = node || document.body;
+
+  var recurseElements = function (className, node) {
+    if (node.classList && node.classList.contains(className)) {
+      Arr.push(node);
+    }
+
+    //childNodes
+    for (var i = 0; i < node.childNodes.length; i++) {
+      recurseElements(className, node.childNodes[i]);
+    }
+  };
+  recurseElements(className, document.body);
+  return Arr;
 };
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,56 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  // var stringify = '';
+
+  if (obj === undefined) {
+    return 'undefined';
+  }
+
+  if (obj === null) {
+    return 'null';
+  }
+
+  if (typeof obj === 'number') {
+    return obj.toString();
+  }
+
+  if (typeof obj === 'boolean') {
+    return obj.toString();
+  }
+
+  if (typeof obj === 'string') {
+    return '"' + obj + '"';
+  }
+
+  if (Array.isArray(obj) && obj.length === 0) {
+    return '[]';
+  }
+
+  if (Array.isArray(obj) && obj.length > 0) {
+    var newArr = '[';
+    for (var i = 0; i < obj.length; i++) {
+      newArr += stringifyJSON(obj[i]);
+      if (i === obj.length - 1) {
+        break;
+      }
+      newArr += ',';
+    }
+    return newArr += ']';
+  }
+
+  if (typeof obj === 'object' && Object.keys(obj).length === 0) {
+    return '{}';
+  }
+
+  if (typeof obj === 'object' && Object.keys(obj).length > 0) {
+    var string = '';
+    for (var key in obj) {
+      if (typeof obj[key] === 'function' || obj[key] === undefined) {
+        return '{}';
+      }
+      string += stringifyJSON(key) + ':' + stringifyJSON(obj[key]) + ',';
+    }
+    return '{' + string.slice(0, string.length - 1) + '}';
+  }
 };
